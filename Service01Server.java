3880fad6c6edc0b9bd84499c82c59b2fd3b98b21
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-04-22 18:29:15 +0100

    1st attempt at gui for service 01

--- Service01Server.java
+++ Service01Server.java
@@ -5,22 +5,73 @@ import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 import java.util.logging.Logger;
 
-public class Service01Server{
-    private int port = 8080;
+public class Service01Server {
+
     private Server server;
-  
+
     private void start() throws IOException {
-      server = ServerBuilder.forPort(port)
-          .addService(new RFIDServiceImpl())
-          .build()
-          .start();
-      System.out.println("Server started, listening on " + port);
-      Runtime.getRuntime().addShutdownHook(new Thread() {
-        @Override
-        public void run() {
-          System.err.println("*** shutting down gRPC server since JVM is shutting down");
-          Service01Server.this.stop();
-          System.err.println("*** server shut down");
-        }
-      });
+        int port = 50051;
+        server = ServerBuilder.forPort(port)
+                .addService(new SecurityRequestServiceImpl())
+                .build()
+                .start();
+        System.out.println("Server started, listening on " + port);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Shutting down gRPC server since JVM is shutting down");
+            Service01Server.this.stop();
+            System.out.println("Server shut down");
+        }));
     }
+
+    private void stop() {
+        if (server != null) {
+            server.shutdown();
+        }
+    }
+
+    private void blockUntilShutdown() throws InterruptedException {
+        if (server != null) {
+            server.awaitTermination();
+        }
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        final Service01Server server = new Service01Server();
+        server.start();
+        server.blockUntilShutdown();
+    }
+
+    private static class SecurityRequestServiceImpl extends SecurityRequestServiceGrpc.SecurityRequestServiceImplBase {
+
+        private String authorisationCode = "234DF501HF8373898900";
+
+        @Override
+        public void requestRFID(RequestRFIDMsg request, StreamObserver<ResponseAccess> responseObserver) {
+            String access;
+            String rfidGiven = request.getCode();
+            char[] rfidArray = rfidGiven.toCharArray();
+            if (rfidGiven.length() != authorisationCode.length()){
+                access = "false";
+            }
+            else {
+                int count = 0;
+                for(int i=0; i<rfidGiven.length()-1 ; i++) {
+                    if (rfidArray[i] != authorisationCode.charAt(i)) {
+                        count = count+1;
+                    }
+                }
+                if (count == 0){
+                    access = "True";
+                }
+                else {
+                    access = "false";
+                }
+            }
+            ResponseAccess response = ResponseAccess.newBuilder()
+                    .setAccessResponse(access)
+                    .build();
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        }
+    }
+}
