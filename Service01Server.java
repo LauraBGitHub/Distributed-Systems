b200f833bc90a4eb6da178ddee38dfc0f926f178
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-04-22 16:49:19 +0100

    GRPC 01 Service 01
    Trying to start creating the first grpc method on service 01

--- Service01Server.java
+++ Service01Server.java
@@ -4,3 +4,23 @@ import io.grpc.stub.StreamObserver;
 
 import java.io.IOException;
 import java.util.logging.Logger;
+
+public class Service01Server{
+    private int port = 8080;
+    private Server server;
+  
+    private void start() throws IOException {
+      server = ServerBuilder.forPort(port)
+          .addService(new RFIDServiceImpl())
+          .build()
+          .start();
+      System.out.println("Server started, listening on " + port);
+      Runtime.getRuntime().addShutdownHook(new Thread() {
+        @Override
+        public void run() {
+          System.err.println("*** shutting down gRPC server since JVM is shutting down");
+          Service01Server.this.stop();
+          System.err.println("*** server shut down");
+        }
+      });
+    }
